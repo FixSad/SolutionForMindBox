5c268d5c196b9e71a9475198473bfee6a0a0b677
FixSad <ost.odino4ka@bk.ru>
2024-09-23 19:35:07 +0300

    class Triangle

---
+++ AreaCalculator/Shapes/Triangle.cs
@@ -0,0 +1,42 @@
+﻿using AreaCalculator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator.Shapes
+{
+    public class Triangle : IShape
+    {
+        private readonly double _a, _b, _c;
+        public bool IsRectangular { get ; private set; }
+        public double Area { get; private set; } 
+
+        public Triangle(double a, double b, double c)
+        {
+            _a = a; _b = b; _c = c;
+            IsRectangular = CalculateIsRectangular();
+        }
+
+        private bool CalculateIsRectangular()
+        {
+            if(Math.Pow(_a, 2) + Math.Pow(_b, 2) == Math.Pow(_c, 2))
+                return true;
+            return false;
+        }
+
+        public void CalculateArea()
+        {
+            double p = (_a + _b + _c) / 2;
+            Area = Math.Sqrt(p * (p - _a) * (p - _b) * (p - _c));
+        }
+
+        public override string ToString()
+        {
+            return $"Shape: {this.GetType().Name}, " +
+                $"Area: {Area}, " +
+                $"Is Rectangular: {IsRectangular}";
+        }
+    }
+}
--- MindBoxSolution/Program.cs
+++ MindBoxSolution/Program.cs
@@ -1,6 +1,6 @@
 ﻿using AreaCalculator.Interfaces;
 using AreaCalculator.Shapes;
 
-IShape circle = new Circle(3);
-circle.CalculateArea();
-Console.WriteLine(circle);
+IShape triangle = new Triangle(4, 3 ,6);
+triangle.CalculateArea();
+Console.WriteLine(triangle);
